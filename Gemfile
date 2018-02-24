6911b78ce9e2d5fa07179c205c61261ddd3b44fa
Guanglin Du <guanglin.du@gmail.com>
2018-02-24 17:41:12 +0800

    Implement contact creation

--- Gemfile
+++ Gemfile
@@ -13,10 +13,11 @@ gem 'x-editable-rails', '1.5.3'
 gem 'rails-i18n',       '4.0.9'
 
 group :development, :test do
-  gem 'byebug',      '4.0.4'
-  gem 'spring',      '2.0.2'
-  gem 'sqlite3',     '1.3.10'
-  gem 'web-console', '2.1.3'
+  gem 'byebug',        '4.0.4'
+  gem 'spring',        '2.0.2'
+  gem 'sqlite3',       '1.3.10'
+  gem 'web-console',   '2.1.3'
+  gem 'awesome_print', '1.6.1'
 end
 
 group :production do
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,4 +10,19 @@ class ContactsController < ApplicationController
   def show
     @contact = Contact.find(params[:id])
   end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:name, :mobile, :phone,:role, :priority,
+                                    :address, :email, :note)
+  end
 end
